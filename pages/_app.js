073828673387af7b925e52f598a8b1be8fdc27ac
David Santos <davidprocode@proton.me>
2023-02-03 16:08:52 +0000

    update 0.1

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '@styles/globals.css'
+import 'semantic-ui-css/semantic.min.css'
+
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { Container } from 'semantic-ui-react'
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Ceep Aureo de Oliveira Filho</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <Container>
+        <Header title="Ceep Aureo de Oliveira Filho" />
+            Hello World!!!
+          </Container>
+        <Footer />
       </main>
-
-      <Footer />
-    </div>
+    </>
   )
 }
